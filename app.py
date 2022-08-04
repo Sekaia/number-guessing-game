536ce3dd5dde05d0c91bc3a4dc45632fb01c1407
Sekaia <gillotte.bm@gmail.com>
2022-08-04 17:43:46 -0500

    Added hints; increased the number of guesses

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import random
 
-GUESSES = 5
+GUESSES = 7
 RANDOM_NUMBER = random.randint(1, 100)
 
 print("Welcome to the number guessing game.")
@@ -8,8 +8,8 @@ print("Welcome to the number guessing game.")
 def get_guess():
     valid = False
     while not valid:
-        guess = input("Guess what the random number is.  ")
-        if int(guess) not in range(1, 100):
+        guess = int(input("Guess what the random number is.  "))
+        if guess not in range(1, 100):
             print("Invalid input")
         else:
             valid = True
@@ -21,10 +21,15 @@ guessed_number = get_guess()
 while GUESSES >= 1:
     if RANDOM_NUMBER == guessed_number:
         print("You win!")
+        break
     elif GUESSES == 1:
         print("You lost! Better luck next time.")
         GUESSES -= 1
-    else:
+    elif guessed_number > RANDOM_NUMBER:
         GUESSES -= 1
-        print("Try again. You have", GUESSES, "more guesses.")
+        print("Too high! You have", GUESSES, "more guesses.")
+        guessed_number = get_guess()
+    elif guessed_number < RANDOM_NUMBER:
+        GUESSES -= 1
+        print("Too low! You have", GUESSES, "more guesses.")
         guessed_number = get_guess()
