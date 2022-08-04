900f64099b27d1f44fa43455a320093a0c813964
Sekaia <gillotte.bm@gmail.com>
2022-08-04 16:39:08 -0500

    added a check for the guessed number

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import random
 
+#TODO: Fix guessed number check
 
-guesses = 5
-random_number = random.randint(1, 100)
+GUESSES = 5
+RANDOM_NUMBER = random.randint(1, 100)
+
 print("Welcome to the number guessing game.")
-guessed_number = input("Guess what the random number is.  ")
 
-while guesses >= 1:
-    if random_number == guessed_number:
-        print("You win!")
-    elif guesses == 1:
-        print("You lost! Better luck next time.")
-        guesses -= 1
+valid = False
+while not valid:
+    guessed_number = input("Guess what the random number is.  ")
+    #why wont this work?
+    if guessed_number in range(1, 100):
+        print("oh")
     else:
-        guesses -= 1
-        print("Try again. You have", guesses, "more guesses.")
+        #always getting this message
+        print("Invalid input")
+
+
+while GUESSES >= 1:
+    if RANDOM_NUMBER == guessed_number:
+        print("You win!")
+    elif GUESSES == 1:
+        print("You lost! Better luck next time.")
+        GUESSES -= 1
+    else:
+        GUESSES -= 1
+        print("Try again. You have", GUESSES, "more guesses.")
         guessed_number = input("Guess what the random number is.  ")
